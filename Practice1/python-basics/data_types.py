cfebcde32ad3d2db69397b9b361ec9079249ba9a
ShakhnozaMamutova <s_mamutova@kbtu.kz>
2026-09-13 16:40:09 +0500

    Add Practice1 Python basics examples

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,19 @@
+x = str("Hello World")
+print(x)
+print(type(x)) 
+
+x = float(20.5)
+print(x)
+print(type(x)) 
+
+x = tuple(("apple", "banana", "cherry"))
+print(x)
+print(type(x)) 
+
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x)) 
+
+x = range(10)
+print(x)
+print(list(x))
---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,19 @@
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+txt = "The best things in life are free!"
+print("free" in txt)
